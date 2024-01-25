c315decee5256bdca7c52b1508eec10b66a318d2
shane-source <157680462+shane-source@users.noreply.github.com>
2024-01-25 10:08:03 +0300

    added files

---
+++ script.js
@@ -0,0 +1,16 @@
+let Navbar = document.querySelector('.navbar');
+let Fabars = document.querySelector('.fa-bars');
+
+
+Fabars.onclick = () =>{
+   Navbar.classList.toggle("active") 
+};
+
+
+
+
+
+
+
+
+        
